c90ef5dd06d3cd8c1caba8a26cf6455b23cd1dab
TonyLee <tonylee51722@gmail.com>
2026-06-19 15:15:38 +0800

    update web icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,10 @@ export default defineNuxtConfig({
       meta: [
         { name: 'theme-color', content: '#090817' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
       ]
     }
   }
